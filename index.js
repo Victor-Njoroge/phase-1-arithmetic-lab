2447a61d1894bdfe440175e331e0fb16356e83d5
Victor <victornjoroge2020@gmail.com>
2023-06-12 23:44:59 +0300

    Done

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+ let multiply = num1*num2;
+
+
+ let random=Math.round(Math.random())+1;
+
+
+ let num3=60;
+ let num4=8;
+
+ const mod=num3%num4;
+
+
+ const max=Math.max(11,16, 5, 7, 20, 15);
